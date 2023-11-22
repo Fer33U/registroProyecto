e3e433eb49c29714bbef2b7ba4220e14563d1f02
unknown <j3sus3gs@gmail.com>
2023-11-22 10:21:11 -0600

    V5

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const registroProyectoController = require('../controllers/registroProyectoController');
+const Proyecto = require('../model/proyecto'); // Importa el modelo de Proyecto
 
 // Mostrar todos los Proyectos (GET)
 router.get('/', registroProyectoController.mostrar);
@@ -17,5 +18,19 @@ router.get('/registroProyecto', (req, res) => {
 });
 // Ruta para contar proyectos (GET)
 router.get('/api/proyectos/count', registroProyectoController.contarProyectos);
+
+// Ruta para consultar los detalles de un proyecto específico
+router.get('/consultaproyectos/:id', async (req, res) => {
+    try {
+        const proyecto = await Proyecto.findById(req.params.id)
+            .populate('stakeholders')
+            .populate('stakeholdersInfo')
+            .populate('pagosParciales');
+
+        res.render('consultaproyectos', { proyecto });
+    } catch (error) {
+        res.status(500).send('Error al cargar los datos del proyecto');
+    }
+});
 
 module.exports = router;
